f436358257be86b2ea1ce63646d729957dde5ced
caraanJC <caraanjohncarlo@gmail.com>
2021-10-13 21:44:44 +0800

    feat: start with blank

--- src/App.js
+++ src/App.js
@@ -1,29 +1,5 @@
-import { Route } from 'react-router-dom';
-import './App.css';
-
-import Header from './components/Header/Header';
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
-import Sidebar from './components/Sidebar/Sidebar';
-import Profile from './pages/Profile/Profile';
-
 function App() {
-    return (
-        <div className='app'>
-            <Header />
-            <Sidebar />
-
-            {/* Modals */}
-            <Login />
-            <Register />
-            {/* Modals */}
-
-            {/* Pages */}
-            <Route exact path='/profile'>
-                <Profile />
-            </Route>
-        </div>
-    );
+    return <div className='app'></div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './state/store';
 
 ReactDOM.render(
     <React.StrictMode>
         <Router>
-            <Provider store={store}>
-                <App />
-            </Provider>
+            <App />
         </Router>
     </React.StrictMode>,
     document.getElementById('root')
