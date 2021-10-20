040e77999dc08dc39561b40002043f4c163d118b
Miguel de Leon <miguelangelodeleon.12@gmail.com>
2021-10-20 23:02:37 +0200

    feat: add shop, navbar (desktop), and Homepage CSS

--- src/App/Shop/Shop.js
+++ src/App/Shop/Shop.js
@@ -1,11 +1,73 @@
 import Navbar from 'shared/Navbar';
 
 const Shop = () => {
-    return (
-        <div>
-            <Navbar />
+  return (
+    <div className="shop">
+      <Navbar />
+      <div className="shop__container">
+        <div className="card">
+          <div className="card__head">
+            <img src="" alt="" />
+          </div>
+          <div className="card__body">
+            <div className="description">
+              <h3>Name</h3>
+              <h4>Php 300</h4>
+            </div>
+            <div className="buttons">
+              <div className="btn btn-accent">Add to Cart</div>
+              <div className="btn btn-border-accent">View</div>
+            </div>
+          </div>
         </div>
-    );
+        <div className="card">
+          <div className="card__head">
+            <img src="" alt="" />
+          </div>
+          <div className="card__body">
+            <div className="description">
+              <h3>Name</h3>
+              <h4>Php 300</h4>
+            </div>
+            <div className="buttons">
+              <div className="btn btn-accent">Add to Cart</div>
+              <div className="btn btn-border-accent">View</div>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card__head">
+            <img src="" alt="" />
+          </div>
+          <div className="card__body">
+            <div className="description">
+              <h3>Name</h3>
+              <h4>Php 300</h4>
+            </div>
+            <div className="buttons">
+              <div className="btn btn-accent">Add to Cart</div>
+              <div className="btn btn-border-accent">View</div>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card__head">
+            <img src="" alt="" />
+          </div>
+          <div className="card__body">
+            <div className="description">
+              <h3>Name</h3>
+              <h4>Php 300</h4>
+            </div>
+            <div className="buttons">
+              <div className="btn btn-accent">Add to Cart</div>
+              <div className="btn btn-border-accent">View</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
