b576d283768736a181f0361e52c1967da7d313cd
Miguel de Leon <miguelangelodeleon.12@gmail.com>
2021-10-21 23:01:11 +0200

    fix: navbar issue

--- src/shared/Navbar/Navbar.js
+++ src/shared/Navbar/Navbar.js
@@ -11,24 +11,17 @@ import UseComponentVisible from 'shared/UseComponentVisible';
 const Navbar = () => {
   const token = useSelector((state) => state.token);
 
-<<<<<<< HEAD
   const openSublist = () => {
-    setIsComponentVisible(true);
+    setIsComponentVisible(!isComponentVisible);
   };
-=======
-    const openSublist = () => {
-        setIsComponentVisible(!isComponentVisible);
-    };
->>>>>>> 2702afe3073c2df0263e6d94504cee48751fd677
 
   const { ref, isComponentVisible, setIsComponentVisible } =
     UseComponentVisible(false);
 
-<<<<<<< HEAD
   return (
     <nav className="navbar">
       <div className="navbar__container">
-        <Link to="/shop" className="navbar__logo">
+        <Link to="/" className="navbar__logo">
           <h1 className="text-accent-dark">Auxilium</h1>
         </Link>
         <div className="navbar__searchbar">
@@ -43,49 +36,24 @@ const Navbar = () => {
                 <img src="" alt="Avatar" />
               </div>
               <p>Username</p>
-=======
-    return (
-        <nav className='navbar'>
-            <div className='navbar__container'>
-                <Link to='/' className='navbar__logo'>
-                    <h1 className='text-accent-dark'>Auxilium</h1>
-                </Link>
-                <div className='navbar__searchbar'>
-                    <input type='text' placeholder='Search...' />
-                    <FiSearch />
-                </div>
-
-                <div className='navbar__options'>
-                    {token ? (
-                        <div>
-                            <div>
-                                <img src='' alt='Avatar' />
-                            </div>
-                            <p>Username</p>
-                        </div>
-                    ) : (
-                        <button className='btn btn-accent'>login</button>
-                    )}
-                    <Link to='/cart'>
-                        <IoCart />
-                    </Link>
-                    <div ref={ref}>
-                        <FaChevronDown onClick={openSublist} />
-                        <ul className='sublist'>
-                            {isComponentVisible && (
-                                <>
-                                    <Link to='/shop'>Change User Details</Link>
-                                    <Link to='/'>Logout</Link>
-                                </>
-                            )}
-                        </ul>
-                    </div>
-                </div>
->>>>>>> 2702afe3073c2df0263e6d94504cee48751fd677
             </div>
           ) : (
             <button className="btn btn-accent">login</button>
           )}
+          <Link to="/cart">
+            <IoCart />
+          </Link>
+          <div ref={ref}>
+            <FaChevronDown onClick={openSublist} />
+            <ul className="sublist">
+              {isComponentVisible && (
+                <>
+                  <Link to="/shop">Change User Details</Link>
+                  <Link to="/">Logout</Link>
+                </>
+              )}
+            </ul>
+          </div>
           <Link to="/cart">
             <IoCart />
           </Link>
