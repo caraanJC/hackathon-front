758167530c7f66615e2dff6af9d9683d14db7290
caraanJC <caraanjohncarlo@gmail.com>
2021-10-22 16:08:57 +0800

    feat: add viewProduct.js to Item.js

--- src/shared/ViewProduct/ViewProduct.js
+++ src/shared/ViewProduct/ViewProduct.js
@@ -1,30 +1,30 @@
-const ViewProduct = () => {
-  return (
-    <div className="modal">
-      <form className="modal__container view">
-        <span>X</span>
-        <div className="view__head">
-          <div className="img">
-            <img src="" alt="" />
-          </div>
-          <div className="desc">
-            <h3>NAME</h3>
-            <h4>Php 300</h4>
-            <button className="btn btn-accent">Add To Cart</button>
-          </div>
+const ViewProduct = ({ item, setIsViewProductOpen }) => {
+    const closeViewProduct = () => {
+        setIsViewProductOpen(false);
+    };
+
+    return (
+        <div className='modal'>
+            <form className='modal__container view'>
+                <span onClick={closeViewProduct} className='close'>
+                    X
+                </span>
+                <div className='view__head'>
+                    <div className='img'>
+                        <img src={item.image} alt={item.name} />
+                    </div>
+                    <div className='desc'>
+                        <h3>{item.name}</h3>
+                        <h4>Php {item.price}</h4>
+                        <button className='btn btn-accent'>Add To Cart</button>
+                    </div>
+                </div>
+                <div className='view__body'>
+                    <p>{item.description}</p>
+                </div>
+            </form>
         </div>
-        <div className="view__body">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
-            repellendus assumenda ex ab, aliquid ullam qui fugiat tenetur
-            laudantium, dolor nemo? Dolor, sequi non vel suscipit enim
-            voluptatem, sed, odio iusto explicabo reiciendis ipsam ipsum!
-            Mollitia alias dolorum voluptatum animi.
-          </p>
-        </div>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default ViewProduct;
